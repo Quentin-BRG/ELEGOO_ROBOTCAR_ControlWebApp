f18746aaa0139620703de4376564dd7b1435192d
Quentin-BRG <berger.quentin@free.fr>
2023-02-20 23:24:24 +0100

    style update

--- index.php
+++ index.php
@@ -9,6 +9,12 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
   </head>
   <body>
+
+    <div id="bluetooth-connection">
+      <button id="bluetooth-connect"><i class='bx bx-power-off'></i></button>
+      <label id="bluetooth-status">Non connecté</label>
+    </div>
+
     <div class="fieldCustom">
         <div class="input-div">
             <label for="column-input">Nombre de colonnes:</label>
@@ -18,6 +24,8 @@
             <button id="refresh-btn">Refresh</button>
         </div>
     </div>
+
+    <table id="field"><tr><td></td><td style="font-weight: bold; text-align: center; vertical-align: middle;">1</td><td style="font-weight: bold; text-align: center; vertical-align: middle;">2</td><td style="font-weight: bold; text-align: center; vertical-align: middle;">3</td><td style="font-weight: bold; text-align: center; vertical-align: middle;">4</td><td style="font-weight: bold; text-align: center; vertical-align: middle;">5</td><td style="font-weight: bold; text-align: center; vertical-align: middle;">6</td></tr><tr><td style="font-weight: bold; text-align: center; vertical-align: middle;">A</td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td></tr><tr><td style="font-weight: bold; text-align: center; vertical-align: middle;">B</td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td></tr><tr><td style="font-weight: bold; text-align: center; vertical-align: middle;">C</td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td></tr><tr><td style="font-weight: bold; text-align: center; vertical-align: middle;">D</td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td></tr></table>
 
     <div class="dashboard">
         <div>
@@ -35,14 +43,7 @@
             <button id="stop">Stop</button>
         </div>
     </div>
-
-    <div id="bluetooth-connection">
-      <button id="bluetooth-connect"><i class='bx bx-power-off'></i></button>
-      <label id="bluetooth-status">Non connecté</label>
-    </div>
     
-<table id="field"><tr><td></td><td style="font-weight: bold; text-align: center; vertical-align: middle;">1</td><td style="font-weight: bold; text-align: center; vertical-align: middle;">2</td><td style="font-weight: bold; text-align: center; vertical-align: middle;">3</td><td style="font-weight: bold; text-align: center; vertical-align: middle;">4</td><td style="font-weight: bold; text-align: center; vertical-align: middle;">5</td><td style="font-weight: bold; text-align: center; vertical-align: middle;">6</td></tr><tr><td style="font-weight: bold; text-align: center; vertical-align: middle;">A</td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td></tr><tr><td style="font-weight: bold; text-align: center; vertical-align: middle;">B</td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td></tr><tr><td style="font-weight: bold; text-align: center; vertical-align: middle;">C</td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td></tr><tr><td style="font-weight: bold; text-align: center; vertical-align: middle;">D</td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td><td style="text-align: center; vertical-align: middle;"></td></tr></table>
-
     <!-- Flag image for the target point -->
     <img id="flag" src="Images\flag.png">
     <!-- Robot image for the robot's position -->
